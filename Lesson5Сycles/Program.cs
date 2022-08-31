04e1236ad30691e4c55313b049d81d4d559a7c5c
Максим Богащенко <sergeyivanov85@yandex.ru>
2022-08-31 18:12:48 +0300

    While

--- Lesson5Сycles/Program.cs
+++ Lesson5Сycles/Program.cs
@@ -8,7 +8,16 @@ using System.Threading.Tasks;
 namespace Lesson5Сycles {
     class Program {
         static void Main(string[] args) {
-
+            const int EXAMPLE_INT = 6;
+            MakeCycleWhile(EXAMPLE_INT);
+            Console.ReadKey();
+        }
+        static void MakeCycleWhile(int exampleInt) {
+            int startInt = 0;
+            while (startInt < exampleInt) {
+                Console.WriteLine(startInt);
+                startInt++;
+            }
         }
     }
 }
