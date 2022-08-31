2e6f529164381f28b760f9c1698b49cfdfa4611c
Максим Богащенко <sergeyivanov85@yandex.ru>
2022-08-31 18:40:02 +0300

    DoWhile

--- Lesson5Сycles/Program.cs
+++ Lesson5Сycles/Program.cs
@@ -9,14 +9,24 @@ namespace Lesson5Сycles {
     class Program {
         static void Main(string[] args) {
             const int EXAMPLE_INT = 6;
+            double min = 2;
+            double max = 8;
             Random rnd = new Random();
             //MakeCycleWhile(EXAMPLE_INT);
-            double myDouble = GetRandomDouble(rnd, 2, 8);
-            Console.WriteLine(myDouble);
+            MakeCycleDoWhile(rnd, min, max, EXAMPLE_INT);
             Console.ReadKey();
         }
+
         static double GetRandomDouble(Random rnd, double min, double max) {
             return rnd.NextDouble() * (max - min) + min;
+        }
+        static void MakeCycleDoWhile(Random rnd, double min, double max, int example) {
+            double myDouble;
+            do {
+                myDouble = GetRandomDouble(rnd, 2, 8);
+                Console.WriteLine(myDouble);
+            }
+            while (myDouble < example);
         }
         static void MakeCycleWhile(int exampleInt) {
             int startInt = 0;
